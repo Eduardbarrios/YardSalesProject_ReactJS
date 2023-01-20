917d2a2fff5712d50917c97e4f34f951172d4007
@Eduard09 <eduardbarrios0903@gmail.com>
2023-01-20 03:51:06 -0500

    fix commit x3

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const { pathToFileURL } = require('url');
 module.exports = {
  entry: './src/index.js',
  output:{
-  path: path.resolve(__dirname, 'build'),
+  path: path.resolve(__dirname, 'dist'),
   filename: 'bundle.js',
   publicPath: '/'
  },
